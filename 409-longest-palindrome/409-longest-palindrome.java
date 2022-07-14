c3086f2bd74dc78188b26bd89b648f6d73d6069a
Anand Raj Singh <99643120+anandrajsingh05@users.noreply.github.com>
2022-07-15 00:25:02 +0530

    Time: 18 ms (6.17%), Space: 42.4 MB (51.80%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -9,9 +9,7 @@ class Solution {
             else map.put(s.charAt(i),1);
         }
         
-        System.out.println(map);
         int sum=0;
-        int count=0;
         for(int a : map.values()){
             if(a%2==0){
                  sum+=a;
