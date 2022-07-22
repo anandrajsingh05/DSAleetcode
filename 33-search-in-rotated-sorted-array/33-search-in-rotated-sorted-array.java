09403e5f3a56044969705d74c2a089935bc2ef08
Anand Raj Singh <99643120+anandrajsingh05@users.noreply.github.com>
2022-07-23 00:49:29 +0530

    Time: 1 ms (74.47%), Space: 41.7 MB (93.10%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,42 +1,23 @@
 class Solution {
     public int search(int[] nums, int target) {
-        
-        if(nums.length==1 && nums[0]==target) return 0;
-       else if(nums.length==1 && nums[0]!=target) return -1;
-        
-        int pivot= pivot(nums);
-        int left=binSearch(nums,target,0,pivot);
-        int right=binSearch(nums,target,pivot+1,nums.length-1);
-        int res=Math.max(left,right);
-        return res;
-      
-    }
-    
-    public int pivot(int[] arr){
-        
         int l=0;
-        int r= arr.length-1;
+        int r=nums.length-1;
         while(l<=r){
             int mid= l+(r-l)/2;
-            if(mid !=arr.length-1 && arr[mid]>arr[mid+1]) return mid;
-            else if(arr[0]>arr[mid]){
-                r=mid-1; 
+            if(nums[mid]==target)return mid;
+            if(nums[mid]>=nums[l]){
+                
+                if(nums[l]<=target && nums[mid]>target){
+                    r=mid-1;
+                }
+                else l=mid+1;                
+                
             }
-            else l=mid+1;
-        }
-       return -1;
-    }
-    
-    public int binSearch(int[] arr, int k, int l, int r){
-       
-        while(l<=r){
-            int mid= l+(r-l)/2;
-            if(arr[mid]==k) return mid;
-            else if(arr[mid]>k){
-                r=mid-1;
+            else{
+                if(nums[l]>target && nums[mid]<=target) l=mid+1;
+                else r=mid-1;
             }
-            else l=mid+1;
         }
-       return -1;
+        return -1;
     }
 }
