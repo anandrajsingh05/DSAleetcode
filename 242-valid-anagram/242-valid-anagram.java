d4e3df267abbbd9e35f76ce9689fb417d4845df6
Anand Raj Singh <99643120+anandrajsingh05@users.noreply.github.com>
2022-08-05 17:43:44 +0530

    Time: 34 ms (9.19%), Space: 46.4 MB (20.12%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,33 +1,35 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-//         Map<Character,Integer> map= new HashMap<>();
-//         for(int i=0; i<s.length(); i++){
-//             Integer count=map.get(s.charAt(i));
-//             map.put(s.charAt(i), count !=null ? count+1 : 1);
-//         }
+        Map<Character,Integer> map= new HashMap<>();
+        for(int i=0; i<s.length(); i++){
+            Integer count=map.get(s.charAt(i));
+            map.put(s.charAt(i), count !=null ? count+1 : 1);
+        }
         
-//          for(int i=0; i<t.length(); i++){
-//             Integer count=map.get(t.charAt(i));
-//             if(count!=null){
-//                 map.put(t.charAt(i),count-1);
-//             }
-//         }
+         for(int i=0; i<t.length(); i++){
+            Integer count=map.get(t.charAt(i));
+            if(count!=null){
+                map.put(t.charAt(i),count-1);
+            }
+        }
         
-//         for(int a : map.values()){
-//             if(a!=0) return false;
-//         }
+        for(int a : map.values()){
+            if(a!=0) return false;
+        }
         
        
-        char[] str1= s.toCharArray();
-        char[] str2= t.toCharArray();
+        // Approach 2-----------------------------
         
-        Arrays.sort(str1);
-        Arrays.sort(str2);
+//         char[] str1= s.toCharArray();
+//         char[] str2= t.toCharArray();
         
-        for(int i=0; i<str1.length; i++){
-             if(str1[i] != str2[i]) return false;
-        }
+//         Arrays.sort(str1);
+//         Arrays.sort(str2);
+        
+//         for(int i=0; i<str1.length; i++){
+//              if(str1[i] != str2[i]) return false;
+//         }
        
         return true;
         
