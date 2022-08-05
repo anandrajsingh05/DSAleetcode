656a0f8255d8b4e60e1bd2f9bf81a47c0d3e0215
Anand Raj Singh <99643120+anandrajsingh05@users.noreply.github.com>
2022-08-05 18:19:16 +0530

    Time: 7 ms (49.91%), Space: 43.3 MB (76.22%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,38 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        Map<Character,Integer> map= new HashMap<>();
+        
+        int[] res= new int[26];
+        
         for(int i=0; i<s.length(); i++){
-           if(map.containsKey(s.charAt(i))){
-               map.put(s.charAt(i), map.get(s.charAt(i))+1);
-           }
-           else map.put(s.charAt(i),1);
+            
+            res[s.charAt(i)-'a']++;
+            res[t.charAt(i)-'a']--;
+            
         }
         
-         for(int i=0; i<t.length(); i++){
-            if(map.containsKey(t.charAt(i))){
-                int count=map.get(t.charAt(i));
-                map.put(t.charAt(i),count-1);
-            }
+        for(int a : res){
+            if(a !=0) return false;
         }
-        
-        for(int a : map.values()){
-            if(a!=0) return false;
-        }
-        
-       
-        // Approach 2-----------------------------
-        
-//         char[] str1= s.toCharArray();
-//         char[] str2= t.toCharArray();
-        
-//         Arrays.sort(str1);
-//         Arrays.sort(str2);
-        
-//         for(int i=0; i<str1.length; i++){
-//              if(str1[i] != str2[i]) return false;
-//         }
-       
         return true;
         
     }
