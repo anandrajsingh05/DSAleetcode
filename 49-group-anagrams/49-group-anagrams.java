21268b0b85a9546141e5370b7ab4c43b8946487f
Anand Raj Singh <99643120+anandrajsingh05@users.noreply.github.com>
2022-08-08 13:00:49 +0530

    Time: 7 ms (95.99%), Space: 45.4 MB (93.50%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -28,21 +28,4 @@ class Solution {
         
     }
     
-//     public boolean isAnagram(String s1, String s2){
-        
-//         if(s1.length() != s2.length()) return false;
-        
-//         int[] arr= new int[26];
-//         for(int i=0; i<s1.length(); i++){
-//             arr[s1.charAt(i)-'a']++;
-//             arr[s2.charAt(i)-'a']--;
-//         }
-        
-//         for(int a : arr){
-//             if(a!=0) return false;
-//         }
-        
-//         return true;
-        
-//     }
 }
