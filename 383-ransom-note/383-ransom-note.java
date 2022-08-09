2ee4e48bafeeecf6720ab4465005c532f457c7bc
Anand Raj Singh <99643120+anandrajsingh05@users.noreply.github.com>
2022-08-09 18:22:54 +0530

    Time: 23 ms (40.25%), Space: 45.9 MB (65.09%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        Map<Character,Integer> map= new HashMap<>();
+        
+        int[] arr= new int[26];
+        
         for(int i=0; i<ransomNote.length(); i++){
-            Integer count=map.get(ransomNote.charAt(i));
-            map.put(ransomNote.charAt(i), count !=null ? count+1 : 1);
+            arr[ransomNote.charAt(i)-'a']++;
+           
         }
         
         for(int i=0; i<magazine.length(); i++){
-            Integer count=map.get(magazine.charAt(i));
-            if(count !=null && count>0){
-                map.put(magazine.charAt(i), count-1);
-            }
+            
+            arr[magazine.charAt(i)-'a']--;
+           
         }
-       
-        for(int a : map.values()){
-            if(a!=0) return false;
+        for(int x : arr)System.out.print(x+" ");
+        for(int a : arr){
+            if(a>0) return false;
+
         }
-        
-        
         return true;
         
     }
