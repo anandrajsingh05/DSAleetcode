6890596f34a7d6dc91a77f871a03065a45361234
Anand Raj Singh <99643120+anandrajsingh05@users.noreply.github.com>
2022-08-10 19:13:13 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (56.43%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -15,14 +15,12 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-      return  solve(root.left,root.right);
-        
+        return solve(root.left, root.right);
     }
-    
     public boolean solve(TreeNode root1, TreeNode root2){
         if(root1==null && root2==null) return true;
         if(root1!=null && root2!=null && root1.val==root2.val){
-            return solve(root1.left, root2.right) && solve(root1.right, root2.left);
+            return solve(root1.left,root2.right) && solve(root1.right, root2.left);
         }
         return false;
     }
