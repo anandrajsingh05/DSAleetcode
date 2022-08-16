55599abf7fc12669779c279645f89cc363d02581
Anand Raj Singh <99643120+anandrajsingh05@users.noreply.github.com>
2022-08-16 18:29:17 +0530

    Time: 2 ms (83.17%), Space: 55 MB (29.08%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -3,8 +3,8 @@ class Solution {
         int index=0;
         for(int i=0; i<nums.length; i++){
             if(nums[i]!=0){
-                nums[index]= nums[i];
-                index++;
+                nums[index++]= nums[i];
+                
             }
         }
         
