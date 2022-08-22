0a373dfe55160ed9095cc1df0d362a483b01845e
Anand Raj Singh <99643120+anandrajsingh05@users.noreply.github.com>
2022-08-22 13:03:02 +0530

    Time: 28 ms (30.15%), Space: 74.4 MB (8.07%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -1,45 +1,56 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        int low= 0;
-        int high=nums.length-1;
-        quickSort(nums,low,high);
+        mergeSort(nums,0,nums.length-1);
+       
         return nums;
     }
-    public void quickSort(int[] arr, int low, int high){
-        
-        if(low<high){
-            int part=partition(arr,low,high);
-            quickSort(arr,low,part-1);
-            quickSort(arr,part+1,high);
+   public void mergeSort(int[] arr, int l, int r){
+        if(l<r){
+            int mid= l+(r-l)/2;
+            mergeSort(arr,l,mid);
+            mergeSort(arr,mid+1,r);
+            merge(arr,l,mid,r);
         }
-    
     }
     
-    public int partition(int[] arr, int low, int high){
+    public void merge(int[] arr, int l,int mid, int r){
        
-        int index= (int)(Math.random()*(high-low)+low);
-        swap(arr,index,low);
-        int pivot=arr[low];
-        int i=low;
-        int j=high;
-        while(i<j){
-            while(arr[i] <= pivot && i<high) i++;
-            while(arr[j] > pivot && j>low) j--;
-            if(i<j){
-                swap(arr,i,j);
+        int[] left= new int[mid-l+1];
+        int[] right= new int[r-mid];
+        int index=0;
+        for(int i=l; i<=mid; i++){
+            left[index++]=arr[i];
+        }
+        
+        index=0;
+        for(int i=mid+1; i<=r; i++){
+            right[index++]=arr[i];
+        }
+       
+        index= l; 
+        int i= 0;
+        int j= 0;
+        while(i<left.length && j<right.length){
+            if(left[i]<=right[j]){
+                arr[index++]=left[i];
+                i++;
+            }
+            else{
+                arr[index++]=right[j];
+                j++;
             }
         }
-        swap(arr,low,j);
         
-        return j;
+        while(i<left.length){
+            arr[index++]=left[i];
+                i++;
+        }
+        
+        while(j<right.length){
+            arr[index++]=right[j];
+                j++;
+        }
         
     }
-    
-    public void swap(int[] arr, int a, int b){
-        int temp=arr[a];
-        arr[a]=arr[b];
-        arr[b]=temp;
-    }
-    
     
 }
